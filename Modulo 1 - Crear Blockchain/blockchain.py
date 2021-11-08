7bc3098c6c12b9225e296300b978d83f0dc76ba2
juambika <juambaperan@gmail.com>
2021-11-08 23:42:08 +0100

    Update blockchain.py

--- Modulo 1 - Crear Blockchain/blockchain.py
+++ Modulo 1 - Crear Blockchain/blockchain.py
@@ -2,8 +2,8 @@
 """
 Created on Mon Nov  8 23:28:55 2021
 
-@author: juan bautista
+@author: Juan Bautista Perán Zamora
 """
 #Modulo 1 - Crear una cadena de bloques - blockchain
 
-#Vamos a programar en Python
+#Vamos a programar en Python 

5b1b8b5e293fa9061fe0b15f164218dff655ce86
juambika <juambaperan@gmail.com>
2021-11-08 23:36:45 +0100

    Creación del Modulo 1

---
+++ Modulo 1 - Crear Blockchain/blockchain.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 23:28:55 2021
+
+@author: juan bautista
+"""
+#Modulo 1 - Crear una cadena de bloques - blockchain
+
+#Vamos a programar en Python

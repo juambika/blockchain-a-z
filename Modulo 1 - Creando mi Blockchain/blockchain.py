601aaded168e9ea25762a0efe4189b72d3103aa1
juambika <juambaperan@gmail.com>
2021-11-10 22:56:16 +0100

    Create blockchain.py

---
+++ Modulo 1 - Creando mi Blockchain/blockchain.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  8 23:28:55 2021
+
+@author: Juan Bautista Perán Zamora
+"""
+#Modulo 1 - Crear una cadena de bloques - blockchain
+
+#Vamos a programar en Python 
+
+#Herramientas para proceder a crear mi Blockchain
+
+# 1 - En anaconda prompt tenemos que poner esta instrucción: pip install Flask==0.12.2
+# 2 - Nos descargamos el Postman para hacer peticiciones de url para la transmisión de datos en https://getpostman.com

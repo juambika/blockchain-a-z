09fcfca4009d60e34a91151f6b171bebddd6eeb7
juambika <juambaperan@gmail.com>
2021-12-10 23:14:52 +0100

    Update blockchain.py

--- Modulo 1 - Creando mi Blockchain/blockchain.py
+++ Modulo 1 - Creando mi Blockchain/blockchain.py
@@ -31,10 +31,12 @@ class Blockchain:
         self.create_block(proof = 1, previous_hash = '0')
         
     def create_block(self, proof, previous_hash):
-        block{'index': len(self.chain)+1,
-              'timestamp': str(datetime.datetime.now()),
-              'proof': proof,
-              'previous_hash': previous_hash
-            }
+        block = {'index': len(self.chain)+1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash
+               }
+        self.chain.append(block)
+        return block
 
 #Parte 2 - Minado de un Bloque de la Cadena
